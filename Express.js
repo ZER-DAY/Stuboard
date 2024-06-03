8172b852e8f7bba267186f1aa4cad901fc7d1b70
ZER-DAY <118398476+ZER-DAY@users.noreply.github.com>
2024-06-03 03:18:17 +0300

    Update Express.js

--- Express.js
+++ Express.js
@@ -7,11 +7,12 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'students_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
+
 
 connection.connect(error => {
     if (error) {
